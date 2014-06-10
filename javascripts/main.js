0f0c8e19b1ebbc0b9b0ac7ee85eb3e461f5a4282
Nathan Ross Powell <nathanrosspowell@gmail.com>
2014-06-09 21:15:34 -0400

    Basic List Template
    * Handlebars.js template
    * jQuery html injection
    * HTML5 boilterplate files

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,8 @@
-console.log('This would be the main JS file.');
+$(window).bind("load", function () {
+    var shoesData = [{name:"Nike", price:199.00 }, {name:"Loafers", price:59.00 }, {name:"Wing Tip", price:259.00 }];
+    //Get the HTML from the template   in the script tag
+    var theTemplateScript = $("#shoe-template").html(); 
+    //Compile the template
+    var theTemplate = Handlebars.compile (theTemplateScript); 
+    $(".shoesNav").append (theTemplate(shoesData)); 
+});
